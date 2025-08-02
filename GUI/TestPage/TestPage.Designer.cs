628109c669a733b4e9a9cbe3f55c1d73abaf53e5
callukcross <123403935+callukcross@users.noreply.github.com>
2025-08-02 13:45:48 -0500

    Added folders in the models folder- PokemonMainArt for where most of the Pokemon pngs are kept, PokemonAltFormArt for the alternate forms of Pokemon not included in MainArt, and PokemonRegionalVariantArt for regonal variant Pokemon not included in MainArt. As of this push, only PokemonMainArt has any images

--- GUI/TestPage/TestPage.Designer.cs
+++ GUI/TestPage/TestPage.Designer.cs
@@ -78,7 +78,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1144, 633);
+            this.ClientSize = new System.Drawing.Size(750, 842);
             this.Controls.Add(this.editProfButton);
             this.Controls.Add(this.loadProfButton);
             this.Controls.Add(this.createProfButton);
